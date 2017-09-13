1110fe7a3e5a4e948e59cff5e8e232c2b2de649e
SiriusSee <Sirius_See@outlook.com>
2017-09-13 18:37:42 +0800

    add idea.md

--- Project1 MinStack/ramdon.cpp
+++ Project1 MinStack/ramdon.cpp
@@ -3,8 +3,10 @@
 #include <cstdio>
 #include <fstream>
 using namespace std;
+
 //genarate some ramdon int from 0-100
 //will be saved in ramdon.txt
+
 int main()
 {
     srand(666);
@@ -13,7 +15,7 @@ int main()
 
     cout << "How many random int do you want?" << endl;
     cin >> number;
-    
+
     for (int i = 0; i < number; ++i)
     {
         os << rand() % 100 << " ";
